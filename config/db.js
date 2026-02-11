bf855e168d5b7e2f6f3bb08922183c2c1a2d49ff
meha7hak <mehak.kotarya786@gmail.com>
2026-02-12 00:57:08 +0530

    error solving

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URL);
 
         console.log("MongoDB Connected Successfully");
     } catch (error) {
